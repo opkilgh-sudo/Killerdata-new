946991792e854ffa3cfbeeb60602d37e4336c00c
opkilgh-sudo <opkilgh@gmail.com>
2026-04-08 22:39:37 +0700

    Update killerdata.lua

--- killerdata.lua
+++ killerdata.lua
@@ -1,6 +1,6 @@
 return {
     ["Slasher"] = {
-        ["M1"] = { -- สกิลที่ 1 (ฟันปกติ)
+        ["M1"] = { -- สกิลหลัก (ฟันปกติ)
             Sounds = {},      
             Animations = {},  
             MaxTime = 0.45,   
@@ -9,7 +9,7 @@ return {
             Size = Vector3.new(4.5, 6, 7.5), 
             Offset = 2.5      
         },
-        ["Skill_1"] = { -- สกิลที่ 2
+        ["Skill_1"] = { -- สกิลที่มีดาเมจอันที่ 1
             Sounds = {}, 
             Animations = {}, 
             MaxTime = 0.45, 
@@ -18,16 +18,7 @@ return {
             Size = Vector3.new(4.5, 6, 7.5), 
             Offset = 2.5
         },
-        ["Skill_2"] = { -- สกิลที่ 3
-            Sounds = {}, 
-            Animations = {}, 
-            MaxTime = 0.45, 
-            Sequences = 16, 
-            KillAt = 15,
-            Size = Vector3.new(4.5, 6, 7.5), 
-            Offset = 2.5
-        },
-        ["Skill_3"] = { -- สกิลที่ 4
+        ["Skill_2"] = { -- สกิลที่มีดาเมจอันที่ 2
             Sounds = {}, 
             Animations = {}, 
             MaxTime = 0.45, 
@@ -36,6 +27,6 @@ return {
             Size = Vector3.new(4.5, 6, 7.5), 
             Offset = 2.5
         }
+        -- สกิลที่ 4 (หาคน) ตัดออกตามคำสั่ง
     }
 }
-
