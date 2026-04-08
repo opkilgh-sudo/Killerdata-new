c9d0f5711a033a4a76b8124c97433f0a73153cb2
opkilgh-sudo <opkilgh@gmail.com>
2026-04-08 22:35:35 +0700

    Create killerdata.lua

---
+++ killerdata.lua
@@ -0,0 +1,41 @@
+return {
+    ["Slasher"] = {
+        ["M1"] = { -- สกิลที่ 1 (ฟันปกติ)
+            Sounds = {},      
+            Animations = {},  
+            MaxTime = 0.45,   
+            Sequences = 16,   
+            KillAt = 15,      
+            Size = Vector3.new(4.5, 6, 7.5), 
+            Offset = 2.5      
+        },
+        ["Skill_1"] = { -- สกิลที่ 2
+            Sounds = {}, 
+            Animations = {}, 
+            MaxTime = 0.45, 
+            Sequences = 16, 
+            KillAt = 15,
+            Size = Vector3.new(4.5, 6, 7.5), 
+            Offset = 2.5
+        },
+        ["Skill_2"] = { -- สกิลที่ 3
+            Sounds = {}, 
+            Animations = {}, 
+            MaxTime = 0.45, 
+            Sequences = 16, 
+            KillAt = 15,
+            Size = Vector3.new(4.5, 6, 7.5), 
+            Offset = 2.5
+        },
+        ["Skill_3"] = { -- สกิลที่ 4
+            Sounds = {}, 
+            Animations = {}, 
+            MaxTime = 0.45, 
+            Sequences = 16, 
+            KillAt = 15,
+            Size = Vector3.new(4.5, 6, 7.5), 
+            Offset = 2.5
+        }
+    }
+}
+
