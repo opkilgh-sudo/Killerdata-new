128cbf776a919d8b08f5754b6dbb25aa6ff663fe
opkilgh-sudo <opkilgh@gmail.com>
2026-04-08 22:40:46 +0700

    Update killerdata.lua

--- killerdata.lua
+++ killerdata.lua
@@ -9,7 +9,7 @@ return {
             Size = Vector3.new(4.5, 6, 7.5), 
             Offset = 2.5      
         },
-        ["Skill_1"] = { -- สกิลที่มีดาเมจอันที่ 1
+        ["Behead"] = { -- สกิลที่มีดาเมจอันที่ 1
             Sounds = {}, 
             Animations = {}, 
             MaxTime = 0.45, 
@@ -18,7 +18,7 @@ return {
             Size = Vector3.new(4.5, 6, 7.5), 
             Offset = 2.5
         },
-        ["Skill_2"] = { -- สกิลที่มีดาเมจอันที่ 2
+        ["Gashing Wound"] = { -- สกิลที่มีดาเมจอันที่ 2
             Sounds = {}, 
             Animations = {}, 
             MaxTime = 0.45, 
