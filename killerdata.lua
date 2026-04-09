bab31da52e39ad6852fd630947ce184bea3e30c9
opkilgh-sudo <opkilgh@gmail.com>
2026-04-09 09:46:46 +0700

    Update killerdata.lua

--- killerdata.lua
+++ killerdata.lua
@@ -1,25 +1,39 @@
 return {
     ["Slasher"] = {
         ["M1"] = { -- สกิลหลัก (ฟันปกติ)
-            Sounds = {},      
+            Sounds = { 
+                ["112809109188560"] = true, ["124903763333174"] = true, 
+                ["116581754553533"] = true, ["107444859834748"] = true, 
+                ["94317217837143"] = true, ["105840448036441"] = true, 
+                ["108907358619313"] = true 
+            },      
             Animations = {},  
-            MaxTime = 0.45,   
-            Sequences = 16,   
+            MaxTime = 0.43,   
+            Sequences = 15,   
             KillAt = 15,      
             Size = Vector3.new(4.5, 6, 7.5), 
             Offset = 2.5      
         },
-        ["Behead"] = { -- สกิลที่มีดาเมจอันที่ 1
+        ["Behead"] = { -- สกิลตัดหัว (ใช้ Animation ID)
             Sounds = {}, 
-            Animations = {}, 
-            MaxTime = 0.45, 
-            Sequences = 16, 
-            KillAt = 15,
+            Animations = { 
+                ["126355327951215"] = true, ["126727756047566"] = true, 
+                ["110702884830060"] = true, ["82113744478546"] = true, 
+                ["126681776859538"] = true 
+            }, 
+            MaxTime = 0.6, 
+            Sequences = 9, 
+            KillAt = 9,
             Size = Vector3.new(4.5, 6, 7.5), 
             Offset = 2.5
         },
-        ["Gashing Wound"] = { -- สกิลที่มีดาเมจอันที่ 2
-            Sounds = {}, 
+        ["Gashing Wound"] = { -- สกิลแผลฉกรรจ์ (ใช้ Sound ID)
+            Sounds = { 
+                ["102228729296384"] = true, ["110372418055226"] = true, 
+                ["72425554233832"] = true, ["86833981571073"] = true, 
+                ["76959687420003"] = true, ["136728245733659"] = true, 
+                ["86494585504534"] = true, ["127793641088496"] = true 
+            }, 
             Animations = {}, 
             MaxTime = 0.45, 
             Sequences = 16, 
@@ -27,6 +41,5 @@ return {
             Size = Vector3.new(4.5, 6, 7.5), 
             Offset = 2.5
         }
-        -- สกิลที่ 4 (หาคน) ตัดออกตามคำสั่ง
     }
 }
